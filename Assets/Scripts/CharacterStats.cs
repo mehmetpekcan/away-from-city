02ae030c9367b04e6a75899eefa0d57c75e73d59
mehmetpekcan <pekcanmehmett@gmail.com>
2021-06-20 15:43:48 +0300

    Add enemy and combat feature

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+    public int maxHealth = 100;
+    public bool isDead;
+
+    public int currentHealth {
+        get;
+        private set;
+    }
+
+    public Stat damage;
+    public Stat armor;   
+
+    void Awake() {
+        currentHealth = maxHealth;
+    }
+
+    void Start() {
+        isDead = false;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.T)){
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage){
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        currentHealth -= damage;
+
+        if (currentHealth <= 0 && !isDead) {
+            isDead = true;
+            Die();
+        } else if (currentHealth > 0) {
+            Debug.Log(transform.name + " takes " + damage + " damage!");
+        }
+    }
+
+
+    public virtual void Die() {
+        Debug.Log(transform.name + " died!");
+    }
+}
